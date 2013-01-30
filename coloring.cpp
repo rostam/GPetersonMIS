06a009dae7f671e96639a92e9ad7c4b0424b21a7
rostam <rostamiev@gmail.com>
2013-01-30 10:46:47 +0100

    boost distributed coloring

--- coloring.cpp
+++ coloring.cpp
@@ -7,6 +7,7 @@
 #include <boost/config.hpp>
 #include <iostream>
 #include <boost/graph/distributed/boman_et_al_graph_coloring.hpp>
+#include <mpi.h>
  
 using namespace boost;
  
@@ -50,14 +51,12 @@ typedef property_map<Graph, vertex_index_t>::const_type vertex_index_map;
 
 int main (int argc, char *argv[])
 {
+    
     mpi::environment env(argc, argv);
     mpi::communicator world;
-    
     Graph g;
     int i =0;
-    if (world.rank() == 0) {
-      //Vertex v1 = add_vertex(g);
-      //Vertex v2 = add_vertex(g);
+    //if (world.rank() == 0) {
       add_edge("1","2",g);
       add_edge("2","3",g);
       add_edge("3","1",g);
@@ -65,9 +64,36 @@ int main (int argc, char *argv[])
       add_edge("1","4",g);
       add_edge("2","4",g);
       add_edge("3","4",g);
-
     
+      add_edge("5","1",g);
+      add_edge("6","1",g);
+      add_edge("7","1",g);
+      
+    //}
+    std::vector<vertices_size_type> color_vec(num_vertices(g));
+    iterator_property_map<vertices_size_type*, vertex_index_map>
+    color(&color_vec.front(), get(vertex_index, g));
+    
+    i = boman_et_al_graph_coloring(g, color);
 
+    if(world.rank() != 0)
+    {
+        MPI_Send(&value, 1, MPI_INT, 0, world.rank, MPI_COMM_WORLD);
+    }
+    else
+    {
+        MPI_RECV(&value,1,
+
+int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag,
+             MPI_Comm comm, MPI_Status *status)
+
+        MPI_RECV
+    }
+    std::cout << i;
+    std::cout << color_vec[0] << " " << color_vec[1] << " " << color_vec[2] << " " << color_vec[3] << " " << color_vec[4] << " " <<std::endl;
+    
+    /*mpi::environment env(argc, argv);
+    mpi::communicator world;
     //synchronize(g.process_group());
     std::vector<vertices_size_type> color_vec(num_vertices(g));
     iterator_property_map<vertices_size_type*, vertex_index_map>
@@ -76,11 +102,12 @@ int main (int argc, char *argv[])
     //boost::property_map< Graph2, Vertex > apm(color_vec);
     
     //VertexNameMap vnm(&color_vec);
-    }
+    
     i = boman_et_al_graph_coloring(g, color);
     std::cout <<i;
+    std::cout << color_vec[0] << color_vec[1] << color_vec[2] <<std::endl;
     //std::vector<int> centralityS(num_vertices(g), 0);
     //CentralityMap centrality(centralityS.begin(), get(vertex_index, g));
-    //brandes_betweenness_centrality(g, centrality);
+    //brandes_betweenness_centrality(g, centrality);*/
     return 0;
 }
